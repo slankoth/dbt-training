9d81f33333d16eba07d1a8033521f59e93d87a85
SUDHARANI.LANKOTHU@GMAIL.COM <SUDHARANI.LANKOTHU@GMAIL.COM>
2023-09-10 20:31:26 +0000

    added delivery_team seed and added to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -20,7 +20,8 @@ c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+d.delivery_team
 
 
 from {{ ref('raw_orders') }} as o
@@ -28,4 +29,7 @@ left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
+
 
